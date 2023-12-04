c5dbb43d659ae68cec2f3c882f2cbd4a75f06a01
Ariel A. Lorenzo <ariel88lorenzo@gmail.com>
2023-12-04 19:34:36 -0300

    add face_recognition

--- app.py
+++ app.py
@@ -1,31 +1,91 @@
 from flask import Flask, render_template, Response
 import cv2
+import face_recognition
+import numpy as np
+import os
+
 
 app = Flask(__name__)
 
-# Abre la conexión con la cámara USB. El número 0 suele ser la cámara predeterminada.
-cap = cv2.VideoCapture(2)
+capturas_dir = 'capturas'
+os.makedirs(capturas_dir, exist_ok=True)
 
-# Verifica si la cámara se abrió correctamente
-if not cap.isOpened():
-    print("Error al abrir la cámara.")
-    exit()
+def guardar_captura(face_id, frame):
+    nombre_archivo = f'captura_face_{face_id}.png'
+    ruta_captura = os.path.join(capturas_dir, nombre_archivo)
+    #print("type:", type(frame))
+    cv2.imwrite(ruta_captura, frame)
+    print(f'Captura guardada: {ruta_captura}')
+    return face_id
 
-def generate_frames():
+def cargar_imagenes_entrenamiento():
+    print("cargar_inagenes")
+    imagenes_entrenamiento = []
+    etiquetas_entrenamiento = []
+
+    for etiqueta in os.listdir(capturas_dir):
+        print(etiqueta)
+        imagen_path = os.path.join(capturas_dir, etiqueta)
+        print(imagen_path)
+        try:
+            imagen = face_recognition.load_image_file(imagen_path)
+            encoding = face_recognition.face_encodings(imagen)[0]
+            imagenes_entrenamiento.append(encoding)
+            etiquetas_entrenamiento.append(etiqueta)
+        except Exception as e:
+            print(str(e))
+
+    print(imagenes_entrenamiento)
+    return imagenes_entrenamiento, etiquetas_entrenamiento
+
+def entrenar_modelo():
+    imagenes_entrenamiento, etiquetas_entrenamiento = cargar_imagenes_entrenamiento()
+    return imagenes_entrenamiento, etiquetas_entrenamiento
+
+def reconocer_rostro(frame, modelo, etiquetas):
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        face_id = f"{top}{right}{bottom}{left}"
+
+        
+           #guardar_captura(face_id, np.array(frame)) 
+        print(type(modelo))
+        matches = face_recognition.compare_faces(modelo, face_encoding)
+        nombre = "Desconocido"
+
+        if True in matches:
+            print("match:" , matches)
+            first_match_index = matches.index(True)
+            print("first_match_index:", first_match_index)
+            nombre = f"{etiquetas[first_match_index]}"
+            print("nombre:", nombre)
+        #else:
+        #    guardar_captura(nombre, frame)
+            
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (255, 0, 0), 2)
+        cv2.putText(frame, nombre, (left, top-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+def generate_frames(modelo, etiquetas):
     while True:
-        # Captura el fotograma de la cámara
         ret, frame = cap.read()
 
-        # Si el fotograma se captura correctamente, conviértelo en formato JPEG
         if ret:
+            #print(modelo, etiquetas)
+            reconocer_rostro(frame, modelo, etiquetas)
+
             ret, buffer = cv2.imencode('.jpg', frame)
             if not ret:
                 break
 
-            # Convierte el búfer a bytes y envíalos como respuesta
             frame_bytes = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
 
 # Ruta para la página principal
 @app.route('/')
@@ -35,9 +95,10 @@ def index():
 # Ruta para la transmisión de video
 @app.route('/video')
 def video():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(modelo, etiquetas), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    # Inicia el servidor Flask en http://localhost:8080/
+    face_appeared = {}
+    modelo, etiquetas = entrenar_modelo()
+    cap = cv2.VideoCapture(2)
     app.run(host='0.0.0.0', port=8080)
-
